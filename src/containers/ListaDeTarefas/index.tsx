6031dad114f8c1d3e2045421c270ccaae11a32fb
Felipe Rodrigues <arthurdz741@gmail.com>
2024-04-26 15:28:36 -0300

    Parametrização com Enums

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,24 +1,26 @@
 import Tarefa from '../../components/Tarefa'
 import { Container } from './styles'
 
+import * as emuns from '../../utils/enums/Tarefa'
+
 const tarefas = [
   {
     titulo: 'Estudar',
     descricao: 'Ver a aula 5',
-    prioridade: 'importante',
-    status: 'pendente'
+    prioridade: emuns.Prioridade.IMPORTANTE,
+    status: emuns.Status.PENDENTE
   },
   {
     titulo: 'Pagar conta',
     descricao: 'Ir ao banco pagar conta de luz',
-    prioridade: 'urgente',
-    status: 'concluída'
+    prioridade: emuns.Prioridade.URGENTE,
+    status: emuns.Status.CONCLUIDA
   },
   {
     titulo: 'Ir para academia',
     descricao: 'Fazer treino de peito',
-    prioridade: 'importante',
-    status: 'pendente'
+    prioridade: emuns.Prioridade.IMPORTANTE,
+    status: emuns.Status.PENDENTE
   }
 ]
 
