363e5edd551ed26e4888ac6ebf5b2b21887be6db
Felipe Rodrigues <arthurdz741@gmail.com>
2024-04-27 17:35:37 -0300

    Criando filtragem de tarefas

--- src/store/reducers/filtro.ts
+++ src/store/reducers/filtro.ts
@@ -2,7 +2,7 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import * as enums from '../../utils/enums/Tarefa'
 
 type FiltroState = {
-  termo: string
+  termo?: string
   criterio: 'prioridade' | 'status' | 'todas'
   valor?: enums.Prioridade | enums.Status
 }
@@ -18,10 +18,14 @@ const FiltroSlice = createSlice({
   reducers: {
     alteraTermo: (state, action: PayloadAction<string>) => {
       state.termo = action.payload
+    },
+    alterarFiltro: (state, action: PayloadAction<FiltroState>) => {
+      state.criterio = action.payload.criterio
+      state.valor = action.payload.valor
     }
   }
 })
 
-export const { alteraTermo } = FiltroSlice.actions
+export const { alteraTermo, alterarFiltro } = FiltroSlice.actions
 
 export default FiltroSlice.reducer
